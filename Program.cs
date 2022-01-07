4ba498f656c6bf46835fff1081dd34947995f9ff
Gustavo <gucruz06@hotmail.com>
2022-01-06 22:42:05 -0300

    first version

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = System.IO.File.ReadAllText(@"C:\Users\gucru\source\repos\SudokuSolver\Sudoku1\Sudoku1.txt");
+            string[,] board = new string[11, 11];
+            int count = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                for (int k = 0; k < 11; k++)
+                {
+                    if (i == 3 | i == 7)
+                    {
+                        board[i, k] = "-";
+                    }
+                    else if (k == 3 | k == 7)
+                    {
+                        board[i, k] = "|";
+                    }
+                    else
+                    {
+                        if (numbers[count].ToString() == "0")
+                        {
+                            board[i, k] = " ";
+                            count++;
+                        }
+                        else
+                        {
+                            board[i, k] = numbers[count].ToString();
+                            count++;
+                        }
+                    }
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    for (int i = 0; i < 11; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < 11; k++)
+                        {
+                            Console.Write(board[i, k] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Digite a célula para Inserir um número(L/C):");
+                    string[] position = (Console.ReadLine().Split(","));
+                    int row = int.Parse(position[0]);
+                    int column = int.Parse(position[1]);
+                    if (column <= 3 & row <= 3)
+                    {
+                        row--;
+                        column--;
+                    }
+                    else if (column > 3 & column < 7 & row <= 3)
+                    {
+                        row--;
+                    }
+                    else if (column >= 7 & row <= 3)
+                    {
+                        row--;
+                        column++;
+                    }
+                    else if (column <= 3 & row > 3 & row < 7)
+                    {
+                        column--;
+                    }
+                    else if (column >= 7 & row > 3 & row < 7)
+                    {
+                        column++;
+                    }
+                    else if (column <= 3 & row >= 7)
+                    {
+                        column--;
+                        row++;
+                    }
+                    else if (column > 3 & column < 7 & row >= 7)
+                    {
+                        row++;
+                    }
+                    else if (column >= 7 & row >= 7)
+                    {
+                        column++;
+                        row++;
+                    }
+                    Console.WriteLine("Número:");
+                    board[row, column] = Console.ReadLine();
+                }
+                catch { }
+            }
+
+        }
+    }
+}
